0da1988497ded4c2ef9cfff2cbcbc332f6deba08
sr.bala <sr.bala.kkl@gmail.com>
2023-09-25 08:34:48 +0530

    imp:Generic

--- Generic.ts
+++ Generic.ts
@@ -3,12 +3,12 @@
 // }
 
 /*********Generic Function*************/
-// function identity<Type>(val:Type):Type{
-//     return val
-// }
-//
-// let v = identity<number>(10);
-// console.log("value",v)
+function identity<Type>(val:Type):Type{
+    return val
+}
+
+let v = identity<number>(10);
+console.log("value",v)
 
 /********** Generic Function **********/
 // Ex 1
@@ -20,6 +20,8 @@ function testFun<T,U extends number>(value1:T,value2:U):object{
     }
 }
 // console.log(testFun(10,'12'))
+console.log(testFun(10,12))
+/****************/
 
 // Ex 2
 const addUserId = <T> (obj:T) => {
@@ -38,6 +40,39 @@ const r =addUserId(user);
 console.log('val = ',r)
 // console.log(`User = ${addUserId(user.name)}`)
 
-console.log()
+/***** Generic Interface ******/
 
+function genericIntFn<T>(arg:T):T{
+    return arg
+}
 
+interface Bottle{
+    brand:string,
+    type:number
+}
+
+console.log(genericIntFn<Bottle>({brand:'asdf',type:10}))
+
+/***** Generic Class *******/
+interface quiz{
+    name:string,
+    type:number
+}
+
+interface course{
+    name:string,
+    author:string,
+    book_number:number
+}
+
+class Sellable<T>{
+    public cart:T[] = [];
+
+    add_to_cart(product:T){
+        this.cart.push(product)
+    }
+}
+
+const myClass = new Sellable<course>();
+myClass.add_to_cart({name:'adsf',author:'bala',book_number:123});
+/*******************/
